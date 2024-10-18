86017a1ca12a3c72fc1e82fa07d227b5fc6c5603
giancarlo-k <giancarlokite@yahoo.com>
2024-10-18 17:45:29 -0400

    Removed GIF sprites from post Unova pokemon

--- pokemon.js
+++ pokemon.js
@@ -4,8 +4,7 @@ import  * as utils  from './scripts/utils.js'
 
 const params = new URLSearchParams(window.location.search);
 const id = parseInt(params.get("id"));
-const shinyIcon = document.querySelector('.shiny-icon')
-const gifIcon = document.querySelector('.gif-icon')
+
 
 async function fetchPokemonInfo() {
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
@@ -19,9 +18,50 @@ async function fetchPokemonInfo() {
   document.title = `Pokédex - ${utils.capitalizeFirstLetter(response.name)}`
   document.querySelector('.sprite-image').src = spriteURL.official.main;
 
-  
-  
-  
+  spriteHandling();
+
+}
+
+fetchPokemonInfo()
+
+const spriteURL = {
+  gif: {
+    main: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`,
+    shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${id}.gif`
+  },
+  official: {
+    main: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/other/official-artwork/${id}.png`,
+    shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/other/official-artwork/shiny/${id}.png`
+  }
+}
+
+const spriteHandling = () => {
+  const shinyIcon = document.querySelector('.shiny-icon');
+  const gifIcon = document.querySelector('.gif-icon');
+
+  gifIcon.addEventListener('click', () => {   
+    const gifIconSrc = gifIcon.src.split('/').pop(); 
+    if (gifIconSrc === 'gif.png') {
+      // NORMAL GIF
+      gifIcon.src = 'images/activated-gif.png';
+      document.querySelector('.sprite-image').style.width = '190px'
+      document.querySelector('.sprite-image').src = spriteURL.gif.main;
+      shinyIcon.src = 'images/empty-sparkling.png';
+    } else if (gifIconSrc === 'activated-gif.png') {
+      // SHINY GIF
+      gifIcon.src = 'images/activated-shiny-gif.png';
+      document.querySelector('.sprite-image').src = spriteURL.gif.shiny;
+      document.querySelector('.sprite-image').style.width = '190px'
+      shinyIcon.src = 'images/empty-sparkling.png';
+    } else if (gifIconSrc === 'activated-shiny-gif.png') {
+      // NORMAL
+      gifIcon.src = 'images/gif.png'
+      document.querySelector('.sprite-image').src = spriteURL.official.main;
+      shinyIcon.src = 'images/empty-sparkling.png';
+      document.querySelector('.sprite-image').style.width = '230px'
+    }
+  })
+
   shinyIcon.addEventListener('click', () => {  
     const shinyIconSrc = shinyIcon.src.split('/').pop(); 
     if (shinyIconSrc === 'empty-sparkling.png') {
@@ -37,37 +77,8 @@ async function fetchPokemonInfo() {
     } 
   });
 
-  gifIcon.addEventListener('click', () => {   
-    const gifIconSrc = gifIcon.src.split('/').pop(); 
-    if (gifIconSrc === 'gif.png') {
-      // NORMAL GIF
-      gifIcon.src = 'images/activated-gif.png';
-      document.querySelector('.sprite-image').src = spriteURL.gif.main;
-      shinyIcon.src = 'images/empty-sparkling.png';
-    } else if (gifIconSrc === 'activated-gif.png') {
-      // SHINY GIF
-      gifIcon.src = 'images/activated-shiny-gif.png';
-      document.querySelector('.sprite-image').src = spriteURL.gif.shiny;
-      shinyIcon.src = 'images/empty-sparkling.png';
-    } else if (gifIconSrc === 'activated-shiny-gif.png') {
-      // NORMAL
-      gifIcon.src = 'images/gif.png'
-      document.querySelector('.sprite-image').src = spriteURL.official.main;
-      shinyIcon.src = 'images/empty-sparkling.png';
-    }
-  })
-}
-
-fetchPokemonInfo()
-
-const spriteURL = {
-  gif: {
-    main: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`,
-    shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${id}.gif`
-  },
-  official: {
-    main: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/other/official-artwork/${id}.png`,
-    shiny: `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/other/official-artwork/shiny/${id}.png`
-  }
-
+  // if its after Unova, hide the gif option
+  if (id > 649) {
+    gifIcon.style.display = 'none'
+  };
 }
